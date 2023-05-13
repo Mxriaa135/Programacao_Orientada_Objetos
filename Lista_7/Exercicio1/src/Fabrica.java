bb30412fbdfee91ed7155e97dc8421766806196c
Mxriaa135 <124539047+Mxriaa135@users.noreply.github.com>
2023-05-12 22:00:06 -0300

    Modulo II

---
+++ Lista_7/Exercicio1/src/Fabrica.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fabrica {
+    private List<Operario> operarios;
+    public Fabrica(){
+        this.operarios = new ArrayList<>();}
+
+    public void adicionarOperario(Operario operario){
+        this.operarios.add(operario);
+    }
+
+    public void iniciarExpediente(){
+        for(Operario operario : operarios){
+            operario.trabalhar();
+        }
+    }
+}
---
+++ Lista_7/Exercicio1/src/OperadorDePrensa.java
@@ -0,0 +1,6 @@
+public class OperadorDePrensa implements Operario {
+    @Override
+    public void trabalhar() {
+        System.out.println("Prensa, prensa, prensa");
+    }
+}

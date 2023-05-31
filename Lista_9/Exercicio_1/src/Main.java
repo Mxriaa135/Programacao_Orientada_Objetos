a45c748dad594ccc6c73e460b7f40c7502a6dbb9
mariaclaracorreamoraes6@gmail.com <mariaclaracorreamoraes6@gmail.com>
2023-05-31 19:06:09 -0300

    Collections e Generics

---
+++ Lista_9/Exercicio_1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Pokedex pokedex = new Pokedex();
+        pokedex.adicionarPokemon(new Pokemon("8jcf5s4", "Charizard", "Fogo"));
+        pokedex.adicionarPokemon(new Pokemon ("9fwe33k", "Pikachu", "Eletric"));
+        pokedex.adicionarPokemon(new Pokemon("34jfsf6", "Squirtle", "Água"));
+    }
+}
---
+++ Lista_9/Exercicio_2/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Liquidificador<Alimento> liquidificador = new Liquidificador<>();
+        liquidificador.bater(new Banana());
+    }
+}
